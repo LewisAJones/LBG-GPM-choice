699b3dfa1d33b99a118de73a40414483d2c4448d
Buffan3369 <lucas.buffan@ens-lyon.fr>
2023-06-22 20:20:57 +0200

    Update mode count scipt, save output

--- R/subscripts/06b_compare_modes.R
+++ R/subscripts/06b_compare_modes.R
@@ -1,9 +1,14 @@
+# Header ----------------------------------------------------------------
+# Project: LBG-GPM-choice
+# File name: 06b_compare_modes.R
+# Description: assess the number of modes in the diversity curves obtained with the 3 GPMs
+# Last updated: 2023-03-07
+# Repository: https://github.com/LewisAJones/LBG-GPM-choice
+# Load libraries --------------------------------------------------------
 library(multimode)
 library(stats)
 library(dplyr)
-
 source("./R/options.R")
-
 ## Load data -------------------------------------------------------------------
 occdf <- readRDS("./data/processed/pbdb_data.RDS")[, c("genus", "max_ma", "min_ma", "bin_assignment", "p_lat_PALEOMAP", "p_lat_GOLONKA", "p_lat_MERDITH2021")]
 #rename time bin column to avoid ambiguities with lat bin one
@@ -27,7 +32,7 @@ assign_rand <- function(x){
   return(rand)
 }
   #assess the number of modes with N independent repeats of adding the random compound
-mode_gathering <- function(subset, N=1){
+mode_gathering <- function(subset, N=10){
   MODES <- c()
   for(i in 1:N){
     #assign each occurrence to a random number between (k-1) and k, where k is its lat bin
@@ -48,16 +53,22 @@ for(mdl in params$models){
   for(t in mode_df$time_bin){
     subset <- occdf[which( (occdf$time_bin_assignment == t) & (is.na(occdf[,paste0(mdl, "_bin")]) == FALSE)),
                     c("genus", paste0(mdl, "_bin"))]
-    # Remove duplicates (same genera present several times within the same time bin)
-    subset <- unique(subset)
-    row.names(subset) <- 1:nrow(subset)
-    mode_df[t, paste0("Nmodes_", mdl)] <- mode_gathering(subset = subset, N=100)
+    if(nrow(subset) == 0){
+      mode_df[which(mode_df$time_bin == t), paste0("Nmodes_", mdl)] <- NA
+    }
+    else{
+      # Remove duplicates (same genera present several times within the same time bin)
+      subset <- unique(subset)
+      row.names(subset) <- 1:nrow(subset)
+      mode_df[which(mode_df$time_bin == t), paste0("Nmodes_", mdl)] <- mode_gathering(subset = subset, N=10)
+    }
   }
 }
-
 ## Save ------------------------------------------------------------------------
 saveRDS(object = mode_df,
         file = "./data/mode_counts.RDS")
+
+
 
 ## For visualisation, subset PALEOMAP in time bin 79 (Danian) ------------------
 subset <- occdf[which( (occdf$time_bin_assignment == 79) & (is.na(occdf$PALEOMAP_bin) == FALSE)),
@@ -73,4 +84,3 @@ subset$rand <- unlist(lapply(X = 1:nrow(subset),
 par(mfrow = c(1,2))
 plot(density(subset$p_lat_PALEOMAP))
 plot(density(subset$rand))
-
